c1fab0f7ee53d289535cde419d388c940d8771a5
John DeSilva <desilvjo@umich.edu>
2014-07-08 16:30:47 -0400

    Add payment_methods/index, closes #14

---
+++ app/admin/payment_method.rb
@@ -0,0 +1,33 @@
+def user_link user
+  link_to "#{user.first_name} #{user.last_name}", [:admin, user]
+end
+
+ActiveAdmin.register PaymentMethod do
+
+  actions :all, :except => [:edit]
+
+  filter :balanced_bank_name
+  filter :balanced_account_number
+
+  index do
+    selectable_column
+    id_column
+    column "Name" do |payment_method|
+      user_link payment_method.user
+    end
+    column :balanced_bank_name
+    column :balanced_account_number
+    actions
+  end
+
+  show do
+    attributes_table do
+      row "Name" do |payment_method|
+        user_link payment_method.user
+      end
+      row :balanced_bank_name
+      row :balanced_account_number
+    end
+  end
+
+end
