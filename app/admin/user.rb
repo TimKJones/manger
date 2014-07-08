63087cd2062f4d477d92c95c0cff11223457dd8c
John DeSilva <desilvjo@umich.edu>
2014-07-08 15:18:43 -0400

    Update users/index, closes #2

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,24 +1,29 @@
 ActiveAdmin.register User do
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+  permit_params :first_name, :last_name, :email
+
   index do
-    column :id
+    selectable_column
+    id_column
     column :first_name
     column :last_name
     column :email
     column :created_at
+    actions
   end
-  
+
+  filter :first_name
+  filter :last_name
+  filter :email
+  filter :created_at
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+    end
+    f.actions
+  end
+
 end
