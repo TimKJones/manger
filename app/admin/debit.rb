d76236df86e705ee6405e2ec023fc5907c67dfea
John DeSilva <desilvjo@umich.edu>
2014-07-18 15:04:08 -0400

    Add debit/show, closes #9, add association to payment_methods, remove edit, closes #10

--- app/admin/debit.rb
+++ app/admin/debit.rb
@@ -1,17 +1,48 @@
+def user_link(user)
+  link_to user, [:admin, user]
+end
+
+def group_link(group)
+  link_to group, [:admin, group]
+end
+
+def payment_method_link(payment_method)
+  link_to payment_method.balanced_account_number, [:admin, payment_method]
+end
+
 ActiveAdmin.register Debit do
+
+  actions :index, :show
+
+  config.batch_actions = false
 
   index do
     selectable_column
     id_column
     column "User" do |debit|
-      link_to debit.user, [:admin, debit.user]
+      user_link debit.user
     end
     column "Group" do |debit|
-      link_to debit.group, [:admin, debit.group]
+      group_link debit.group
     end
     #TODO enum => to_s?
     column :status
     column :created_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row "User" do
+        user_link debit.user
+      end
+      row "Group" do
+        group_link debit.group
+      end
+      row "Payment Method" do
+        payment_method_link debit.payment_method
+      end
+    end
   end
 
 end
--- app/models/debit.rb
+++ app/models/debit.rb
@@ -2,4 +2,5 @@ class Debit < ActiveRecord::Base
   belongs_to :user
   belongs_to :debit_group
   delegate :group, to: :debit_group
+  belongs_to :payment_method
 end
