4ab67ba803f545deaf25d46fbced847169124980
John DeSilva <desilvjo@umich.edu>
2014-07-23 10:29:53 -0400

    Add amount to credit model with hype map reduce

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,5 +1,10 @@
 class Credit < ActiveRecord::Base
   belongs_to :debit_group
-  belongs_to :group, through: :debit_group
-  belongs_to :company, through: :group
+  delegate :group, to: :debit_group
+  delegate :company, to: :group
+
+  def amount
+    debit_group.debits.map(&:rent_amount).reduce(:+)
+  end
+
 end
