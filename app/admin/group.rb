c99e9a0937b1f03300036a207deab79e66437b46
John DeSilva <desilvjo@umich.edu>
2014-07-28 22:56:09 -0400

    Add groups/show (-users), closes #6

--- app/admin/group.rb
+++ app/admin/group.rb
@@ -21,4 +21,54 @@ ActiveAdmin.register Group do
     actions
   end
 
+  show do
+    attributes_table do
+      row :street_address
+      row :city
+      row "Rent Amount" do
+        "hashtag TODO"
+      end
+      row "Company" do
+        company_link group.company
+      end
+      row "Pay Date" do
+        "hashtag TODO"
+      end
+      row :first_payment_date
+      row :final_payment_date
+      row :invite_token do
+        link_to "https://rentpay.cribspot.com/#/join/#{group.invite_token}"
+      end
+    end
+
+    panel "Invites" do
+      table_for group.invites do
+        column :email
+        column :name
+        column :created_at
+      end
+    end
+
+    panel "Past Payments" do
+      table_for group.debit_groups do
+        column :amount
+        column :created_at
+        column "Status" do
+          "#TODO"
+        end
+      end
+    end
+
+=begin
+    #TODO how are users related to groups?
+    panel "Users" do
+      table_for group.users do
+        column :first_name
+        column :last_name
+        column :email
+      end
+    end
+=end
+  end
+
 end
